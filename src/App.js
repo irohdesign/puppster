84f624e5f83d589be46c849e111451631aa0c0d2
Jalia Evans <irohdesign@outlook.com>
2019-02-12 13:46:52 -0500

    fixed issues with navigation

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import NavTabs from "./components/NavTabs";
-import navbar from "./components/navbar";
+import Navbar from "./components/navbar";
 import about from "./components/about";
 import discover from "./components/discover";
 import search from "./components/search";
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <Router>
       <div>
-        <NavTabs />
-        <Route exact path="/" component={navbar} />
+        <Navbar />
         <Route exact path="/about" component={about} />
         <Route exact path="/blog" component={discover} />
         <Route path="/contact" component={search} />
